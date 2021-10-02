24b0f32fde5b9c9b5c8d67e773a42eaf573199fe
Nirmal Karthikeyan <nirmalkarthikeyan1503@gmail.com>
2021-10-02 20:24:15 +0530

    Login basics implemented

---
+++ lib/signup.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Signup extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Cool app name',
+      home: Signup_Class(),
+    );
+  }
+}
+
+class Signup_Class extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
