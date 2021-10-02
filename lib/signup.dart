c940585d2e66beabbd2b27934d14c919cecb4f69
Nirmal Karthikeyan <nirmalkarthikeyan1503@gmail.com>
2021-10-02 21:08:50 +0530

    Signup page sorta done

--- lib/signup.dart
+++ lib/signup.dart
@@ -16,6 +16,129 @@ class Signup_Class extends StatelessWidget{
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color(0xffF3EAC2),
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 100,
+              ),
+              Text('Sign Up!',
+              style: TextStyle(
+                  fontSize: 30,
+                  color: Color(0xffEC524B),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Center(
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width*0.8,
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20))
+                      ),
+                      hintText: 'Enter a nice, unique username!',
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Center(
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width*0.8,
+                  child: TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20))
+                      ),
+                      hintText: 'E-mail?',
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Center(
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width*0.8,
+                  child: TextField(
+                    keyboardType: TextInputType.visiblePassword,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20))
+                      ),
+                      hintText: 'Secret, super safe password here.',
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Center(
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width*0.8,
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20))
+                      ),
+                      hintText: 'Where do you live?',
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Center(
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width*0.8,
+                  child: TextField(
+                    keyboardType: TextInputType.phone,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20))
+                      ),
+                      hintText: 'Phone number :)',
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Center(
+                child: ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25.0)
+                    ),
+                    primary: Color(0xffEC524B),
+                    padding: EdgeInsets.all(15),
+                    onPrimary: Color(0xffF3EAC2),
+                    elevation:5,
+                  ),
+                  label: Text('Sign Up'),
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Signup()));
+                  },
+                  icon: Icon(Icons.assignment_turned_in),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
