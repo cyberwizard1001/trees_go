4e59d7962e8bcb793369ae32f39ff4185318dc31
Sumithra-Sudhakar <sumisooriya@gmail.com>
2021-10-03 16:52:02 +0530

    Sumithra-nirmallog

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -19,149 +19,168 @@ class _LoginPageState extends State<LoginPage> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          backgroundColor: Color(0xffF3EAC2),
-          body: SafeArea(
-            child: Padding(
-                padding: EdgeInsets.all(10),
-                child: ListView(
-                  children: <Widget>[
-                    SizedBox(
-                      height: 100,
-                    ),
-                    Container(
-                        alignment: Alignment.center,
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          'Sign in',
-                          style: TextStyle(
-                            fontSize: 30,
-                            color: Color(0xffEC524B),
-                          ),
-                        )),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(10),
-                      child: TextField(
-                        cursorColor: Color(0xffEC524B),
-                        controller: nameController,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(borderRadius: BorderRadius.all(
-                            Radius.circular(20),
-                          ),),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(20)),
-                            borderSide: const BorderSide(
-                                color: Color(0xffEC524B), width: 2.0),
-                          ),
-                          labelText: 'Username',
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                      padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-                      child: TextField(
-                        cursorColor: Color(0xffEC524B),
-                        obscureText: true,
-                        controller: passwordController,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(20)),
-                            borderSide: const BorderSide(
-                                color: Color(0xffEC524B), width: 2.0),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(20)),
-                            borderSide: const BorderSide(
-                                color: Color(0xffEC524B), width: 2.0),
-                          ),
-                          labelText: 'Password',
-                        ),
-                      ),
-                    ),
-                    FlatButton(
-                      onPressed: () {
-                        //forgot password screen
-                      },
-                      textColor: Color(0xffF3EAC2),
-                      child: Text(
-                        'Forgot Password',
-                        style: TextStyle(
-                          color: Color(0xffC0791E),
-                        ),
-                      ),
-                    ),
-                    Container(
-                        height: 50,
-                        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                        child: SizedBox(
-                          child: ElevatedButton.icon(
-                            label: Text('Login'),
-                            icon: Icon(Icons.keyboard_arrow_right_sharp),
-                            style: ElevatedButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(25.0)),
-                              primary: Color(0xffEC524B),
-                              padding: EdgeInsets.all(15),
-                              onPrimary: Color(0xffF3EAC2),
-                              elevation: 5,
-                            ),
-                            onPressed: () {
-                              Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (context) => Choice()));
-                              print(nameController.text);
-                              print(passwordController.text);
-                            },
-                          ),
-                        )),
-                    Container(
-                      height: 30,
-                    ),
-                    Container(
-                        child: Row(
+        resizeToAvoidBottomInset: false,
+        body: Stack(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/furry_friends.png'),
+                  )),
+            ),
+            Container(
+              child: SafeArea(
+                child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: ListView(
                       children: <Widget>[
-                        Text(
-                          'Do not have an account?',
-                          style: TextStyle(
-                            color: Color(0xffC0791E),
+                        SizedBox(
+                          height: 200,
+                        ),
+                        Container(
+                            alignment: Alignment.center,
+                            padding: EdgeInsets.all(10),
+                            child: Text(
+                              'Sign in',
+                              style: TextStyle(
+                                fontSize: 30,
+                                color: Color(0xffEC524B),
+                              ),
+                            )),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: TextField(
+                            cursorColor: Color(0xffEC524B),
+                            controller: nameController,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(20),
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius:
+                                BorderRadius.all(Radius.circular(20)),
+                                borderSide: const BorderSide(
+                                    color: Color(0xffEC524B), width: 2.0),
+                              ),
+                              labelText: 'Username',
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                          child: TextField(
+                            cursorColor: Color(0xffEC524B),
+                            obscureText: true,
+                            controller: passwordController,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius:
+                                BorderRadius.all(Radius.circular(20)),
+                                borderSide: const BorderSide(
+                                    color: Color(0xffEC524B), width: 2.0),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius:
+                                BorderRadius.all(Radius.circular(20)),
+                                borderSide: const BorderSide(
+                                    color: Color(0xffEC524B), width: 2.0),
+                              ),
+                              labelText: 'Password',
+                            ),
                           ),
                         ),
                         FlatButton(
-                          textColor: Colors.blue,
-                          child: Container(
+                          onPressed: () {
+                            //forgot password screen
+                          },
+                          textColor: Color(0xffF3EAC2),
+                          child: Text(
+                            'Forgot Password',
+                            style: TextStyle(
+                              color: Color(0xffC0791E),
+                            ),
+                          ),
+                        ),
+                        Container(
+                            height: 50,
+                            padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                            child: SizedBox(
+                              child: ElevatedButton.icon(
+                                label: Text('Login'),
+                                icon: Icon(Icons.keyboard_arrow_right_sharp),
+                                style: ElevatedButton.styleFrom(
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius:
+                                      BorderRadius.circular(25.0)),
+                                  primary: Color(0xffEC524B),
+                                  padding: EdgeInsets.all(15),
+                                  onPrimary: Color(0xffF3EAC2),
+                                  elevation: 5,
+                                ),
+                                onPressed: () {
+                                  Navigator.of(context).push(MaterialPageRoute(
+                                      builder: (context) => Choice()));
+                                  print(nameController.text);
+                                  print(passwordController.text);
+                                },
+                              ),
+                            )),
+                        Container(
+                          height: 30,
+                        ),
+                        Container(
                             child: Row(
-                              children: [
+                              children: <Widget>[
                                 Text(
-                                  'Sign Up',
+                                  'Do not have an account?',
                                   style: TextStyle(
-                                    fontSize: 20,
                                     color: Color(0xffC0791E),
                                   ),
                                 ),
-                                Container(
-                                  width: 10,
-                                ),
-                                Icon(Icons.arrow_drop_down_circle_outlined,
-                                    color: Color(0xffC0791E))
+                                FlatButton(
+                                  textColor: Colors.blue,
+                                  child: Container(
+                                    child: Row(
+                                      children: [
+                                        Text(
+                                          'Sign Up',
+                                          style: TextStyle(
+                                            fontSize: 20,
+                                            color: Color(0xffC0791E),
+                                          ),
+                                        ),
+                                        Container(
+                                          width: 10,
+                                        ),
+                                        Icon(Icons.arrow_drop_down_circle_outlined,
+                                            color: Color(0xffC0791E))
+                                      ],
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                    Navigator.of(context).push(MaterialPageRoute(
+                                        builder: (context) => Signup()));
+                                    //signup screen
+                                  },
+                                )
                               ],
-                            ),
-                          ),
-                          onPressed: () {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => Signup()));
-                            //signup screen
-                          },
-                        )
+                              mainAxisAlignment: MainAxisAlignment.center,
+                            ))
                       ],
-                      mainAxisAlignment: MainAxisAlignment.center,
-                    ))
-                  ],
-                )),
-          )),
+                    )),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
