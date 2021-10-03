e5987ee03fda6a6f8f6ecfc07da26703fbbc7802
Nirmal Karthikeyan <nirmalkarthikeyan1503@gmail.com>
2021-10-03 14:43:38 +0530

    Improved Animals.dart

--- lib/Animals.dart
+++ lib/Animals.dart
@@ -30,88 +30,94 @@ class _AnimalsState extends State<Animals> {
           padding: EdgeInsets.all(10),
           child: ListView(
             children: [
-              ElevatedButton(
-                  onPressed: () {},
-                  onLongPress: () {},
-                  style: ButtonStyle(
-                      backgroundColor:
+              SizedBox(
+                height: 100,
+              ),
+              Row(
+                children: [
+                  ElevatedButton(
+                      onPressed: () {},
+                      onLongPress: () {},
+                      style: ButtonStyle(
+                          backgroundColor:
                           MaterialStateProperty.all<Color>(Color(0xff9AD3BC)),
-                      elevation: MaterialStateProperty.all<double>(20),
-                      shadowColor:
+                          elevation: MaterialStateProperty.all<double>(20),
+                          shadowColor:
                           MaterialStateProperty.all<Color>(Color(0xffEC524B)),
-                      shape: MaterialStateProperty.all<OutlinedBorder>(
-                          CircleBorder())),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(100),
-                    child: Image.asset(
-                      'assets/dog.jpg',
-                      height: 200,
-                      width: 200,
-                    ),
-                  )),
-              Container(
-                height: 30,
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).push(
-                        MaterialPageRoute(builder: (context) => CardsView()));
-                  },
-                  onLongPress: () {},
-                  style: ButtonStyle(
-                      backgroundColor:
+                          shape: MaterialStateProperty.all<OutlinedBorder>(
+                              CircleBorder())),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(100),
+                        child: Image.asset(
+                          'assets/dog.jpg',
+                          height: 150,
+                          width: 150,
+                        ),
+                      )),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                            MaterialPageRoute(builder: (context) => CardsView()));
+                      },
+                      onLongPress: () {},
+                      style: ButtonStyle(
+                          backgroundColor:
                           MaterialStateProperty.all<Color>(Color(0xff9AD3BC)),
-                      elevation: MaterialStateProperty.all<double>(20),
-                      shape: MaterialStateProperty.all<OutlinedBorder>(
-                          CircleBorder())),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(100),
-                    child: Image(
-                      image: AssetImage('assets/cat.jpg'),
-                      height: 200,
-                      width: 200,
-                    ),
-                  )),
-              Container(
-                height: 30,
+                          elevation: MaterialStateProperty.all<double>(20),
+                          shape: MaterialStateProperty.all<OutlinedBorder>(
+                              CircleBorder())),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(100),
+                        child: Image(
+                          image: AssetImage('assets/cat.jpg'),
+                          height: 150,
+                          width: 150,
+                        ),
+                      )),
+                ],
               ),
-              ElevatedButton(
-                  onPressed: () {},
-                  onLongPress: () {},
-                  style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Color(0xff9AD3BC)),
-                      elevation: MaterialStateProperty.all<double>(20),
-                      shape: MaterialStateProperty.all<OutlinedBorder>(
-                          CircleBorder())),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(100),
-                    child: Image.asset(
-                      'assets/parrot.jpg',
-                      height: 200,
-                      width: 200,
-                    ),
-                  )),
-              Container(
-                height: 30,
+              SizedBox(
+                height: 50,
               ),
-              ElevatedButton(
-                  onPressed: () {},
-                  onLongPress: () {},
-                  style: ButtonStyle(
-                      backgroundColor:
+              Row(
+                children: [
+                  ElevatedButton(
+                      onPressed: () {},
+                      onLongPress: () {},
+                      style: ButtonStyle(
+                          backgroundColor:
                           MaterialStateProperty.all<Color>(Color(0xff9AD3BC)),
-                      elevation: MaterialStateProperty.all<double>(20),
-                      shape: MaterialStateProperty.all<OutlinedBorder>(
-                          CircleBorder())),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(100),
-                    child: Image.asset(
-                      'assets/others.jpg',
-                      height: 200,
-                      width: 200,
-                    ),
-                  )),
+                          elevation: MaterialStateProperty.all<double>(20),
+                          shape: MaterialStateProperty.all<OutlinedBorder>(
+                              CircleBorder())),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(100),
+                        child: Image.asset(
+                          'assets/parrot.jpg',
+                          height: 150,
+                          width: 150,
+                        ),
+                      )),
+                  ElevatedButton(
+                      onPressed: () {},
+                      onLongPress: () {},
+                      style: ButtonStyle(
+                          backgroundColor:
+                          MaterialStateProperty.all<Color>(Color(0xff9AD3BC)),
+                          elevation: MaterialStateProperty.all<double>(20),
+                          shape: MaterialStateProperty.all<OutlinedBorder>(
+                              CircleBorder())),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(100),
+                        child: Image.asset(
+                          'assets/others.jpg',
+                          height: 150,
+                          width: 150,
+                        ),
+                      )),
+                ],
+              )
+
             ],
           ),
         ),
